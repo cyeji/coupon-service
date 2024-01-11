186bd5b844efc6696361e7178d53a70a410f920f
cyeji <choyeji1591@gmail.com>
2024-01-11 22:08:37 +0900

    test : 날짜 변환 테스트

---
+++ src/test/java/com/yeji/couponservice/repository/entity/CouponTest.java
@@ -0,0 +1,25 @@
+package com.yeji.couponservice.repository.entity;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CouponTest {
+
+    private LocalDate convertToDate(String date) {
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyyMMdd"));
+    }
+
+
+    @Test
+    @DisplayName("날짜 변환 테스트")
+    void test_case_1() throws Exception {
+        // given
+        String date = "20240111";
+        // when
+        LocalDate localDate = convertToDate(date);
+        // then
+        System.out.println(localDate);
+    }
+}
