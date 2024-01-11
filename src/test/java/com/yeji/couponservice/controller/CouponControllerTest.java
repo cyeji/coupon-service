e8acae99d940661cc1034cff1297a6fde9dcc9a7
cyeji <choyeji1591@gmail.com>
2024-01-11 22:08:27 +0900

    test : 쿠폰 생성 테스트

---
+++ src/test/java/com/yeji/couponservice/controller/CouponControllerTest.java
@@ -0,0 +1,48 @@
+package com.yeji.couponservice.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.yeji.couponservice.controller.form.CouponRequestForm;
+import com.yeji.couponservice.repository.entity.enums.CouponType;
+import com.yeji.couponservice.repository.entity.enums.DiscountType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class CouponControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Test
+    @DisplayName("쿠폰 생성 요청 - 200")
+    void test_case_1() throws Exception {
+        // given
+        CouponRequestForm couponRequestForm = new CouponRequestForm();
+        couponRequestForm.setCouponName("선착순 이벤트 쿠폰");
+        couponRequestForm.setCouponCode("네고왕");
+        couponRequestForm.setCost(10);
+        couponRequestForm.setNumberOfCoupons(100);
+        couponRequestForm.setDiscountType(DiscountType.PERCENT.name());
+        couponRequestForm.setCouponType(CouponType.ALL.name());
+        couponRequestForm.setDownloadStartDate("20240111");
+        couponRequestForm.setDownloadEndDate("20240120");
+        couponRequestForm.setAvailableStartDate("20240111");
+        couponRequestForm.setAvailableEndDate("20240120");
+        // when
+
+        // then
+        mockMvc.perform(post("/api/coupon").contentType(MediaType.APPLICATION_JSON)
+                                           .content(mapper.writeValueAsString(couponRequestForm)))
+               .andExpect(status().isOk());
+    }
+}
